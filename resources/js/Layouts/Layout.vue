ad3d0d760852cdbfe4d480cf8a46e19c9f21870d
nolan <nolan@ncphi.com>
2025-03-16 08:36:19 -0300

    Fix stylign

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,15 +1,11 @@
 <template>
-  <div
-    class="min-h-full bg-gradient-to-br from-pink-100 via-blue-100 to-purple-100"
-  >
-    <Heading />
-    <main>
-      <slot />
-    </main>
-    <footer class="px-4 py-6 text-gray-800 italic text-center">
-      Made by Nolan; Supervised by Ellen.
-    </footer>
-  </div>
+  <Heading />
+  <main>
+    <slot />
+  </main>
+  <footer class="px-4 py-6 text-gray-800 italic text-center">
+    Made by Nolan; Supervised by Ellen.
+  </footer>
 </template>
 <script setup lang="ts">
 import Heading from "@/Components/Heading.vue";
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.ts')
     @inertiaHead
 </head>
-<body>
+<body
+        class="min-h-full bg-gradient-to-br from-pink-100 via-blue-100 to-purple-100"
+>
 @inertia
 </body>
 </html>

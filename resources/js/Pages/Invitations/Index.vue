6f5024cb285c744da0983fbb50145bb13040c135
nolan <nolan@ncphi.com>
2025-03-18 20:56:55 -0300

    link to the rsvp

--- resources/js/Pages/Invitations/Index.vue
+++ resources/js/Pages/Invitations/Index.vue
@@ -5,8 +5,10 @@
       <Card>
         <h2 class="heading text-3xl mb-3">Invitations</h2>
         <div v-for="invitation in invitations" class="w-full">
-          <h3 class="heading text-4xl mb-2">
-            {{ invitation.slug }}
+          <h3 class="heading text-4xl mb-2 hover:underline">
+            <x-link :href="route('rsvp.show', { slug: invitation.slug })">
+              {{ invitation.slug }}
+            </x-link>
           </h3>
           <ul>
             <li
@@ -34,6 +36,8 @@ import Layout from "@/Layouts/Layout.vue";
 import { Head } from "@inertiajs/vue3";
 import Invitation = App.Models.Invitation;
 import Card from "@/Components/Card.vue";
+import { route } from "ziggy-js";
+import XLink from "@/Components/x-link.vue";
 
 type InvitationWithGuests = Invitation & {
   guests: App.Models.Guest[];
